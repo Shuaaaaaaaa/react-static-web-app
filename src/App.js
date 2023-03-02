b7fdc11a8345ac2b7616fd57aabc4f536d84647c
shuaaaa <shua.noh@outlook.com>
2023-03-02 14:43:07 +0900

    restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
-
-  return <div>{data}</div>;
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
 export default App;
